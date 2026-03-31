279b7025f27d0354477fa9639536a164b55f968d
Moebytes <pie4chan@gmail.com>
2026-03-31 13:36:01 -0400

    wrong path again

--- afterpack.js
+++ afterpack.js
@@ -2,13 +2,13 @@ const fs = require("fs")
 const path = require("path")
 
 exports.default = async function (context) {
-    if (context.electronPlatformName !== "darwin") return
+    if (context.packager.platform.name !== "mac") return
 
     const appPath = context.appOutDir
     const appName = context.packager.appInfo.productFilename + ".app"
 
-    const ffmpegPath = path.join(appPath, appName, "ffmpeg/ffmpeg")
-    const ffprobePath = path.join(appPath, appName, "ffmpeg/ffprobe")
+    const ffmpegPath = path.join(appPath, appName, "Contents/ffmpeg/ffmpeg")
+    const ffprobePath = path.join(appPath, appName, "Contents/ffmpeg/ffprobe")
 
     fs.chmodSync(ffmpegPath, 0o755)
     fs.chmodSync(ffprobePath, 0o755)
